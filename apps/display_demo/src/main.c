11269f264bcd578e1ee5efa66e27087b179a7dc3
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2020-10-25 13:24:49 +0100

    Moved rcl code to seperate thread
    This is useful, as it frees up the main thread to deal with things that
    are only possible there (e.g. display manipulation on native_posix).

--- apps/display_demo/src/main.c
+++ apps/display_demo/src/main.c
@@ -1,59 +1,9 @@
-#include <rcl/rcl.h>
-#include <rclc/rclc.h>
-#include <zephyr.h>
-
 #include "status.h"
-
-#define RCCHECK(fn)                      \
-  {                                      \
-    rcl_ret_t temp_rc = fn;              \
-    if ((temp_rc != RCL_RET_OK)) {       \
-      atomic_set(&status, STATUS_ERROR); \
-      return;                            \
-    }                                    \
-  }
-#define RCSOFTCHECK(fn)            \
-  {                                \
-    rcl_ret_t temp_rc = fn;        \
-    if ((temp_rc != RCL_RET_OK)) { \
-    }                              \
-  }
 
 void main(void) {
   atomic_set(&status, STATUS_CONNECTING);
 
-  rcl_allocator_t allocator = rcl_get_default_allocator();
-
-  // Initialize support. The loop with error handling is required, as the
-  // initialization will fail after some time if no agent is connected
-  // (on my stm32f429i-disc1 after ~55 seconds).
-  rclc_support_t support;
   while (true) {
-    rcl_ret_t support_init_ret =
-        rclc_support_init(&support, 0, NULL, &allocator);
-
-    if (support_init_ret == RCL_RET_OK) {
-      break;
-    } else {
-      rclc_support_fini(&support);
-      if (support_init_ret != RCL_RET_ERROR) {
-        atomic_set(&status, STATUS_ERROR);
-        return;
-      }
-    }
-
     k_msleep(100);
   }
-
-  rcl_node_t node = rcl_get_zero_initialized_node();
-  RCCHECK(rclc_node_init_default(&node, "microros_zephyr_display_demo", "",
-                                 &support));
-
-  atomic_set(&status, STATUS_RUNNING);
-  while (1) {
-    k_msleep(1000);
-  }
-
-  RCCHECK(rcl_node_fini(&node));
-  RCCHECK(rclc_support_fini(&support));
 }
---
+++ apps/display_demo/src/uros.c
@@ -0,0 +1,68 @@
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <zephyr.h>
+
+#define UROS_THREAD_STACK_SIZE 16384
+#define UROS_THREAD_PRIORITY 3
+
+#define RCCHECK(fn)                      \
+  {                                      \
+    rcl_ret_t temp_rc = fn;              \
+    if ((temp_rc != RCL_RET_OK)) {       \
+      atomic_set(&status, STATUS_ERROR); \
+      return;                            \
+    }                                    \
+  }
+#define RCSOFTCHECK(fn)            \
+  {                                \
+    rcl_ret_t temp_rc = fn;        \
+    if ((temp_rc != RCL_RET_OK)) { \
+    }                              \
+  }
+
+#include "status.h"
+
+void uros_thread(void *param1, void *param2, void *param3);
+K_THREAD_DEFINE(uros_thread_tid, UROS_THREAD_STACK_SIZE, &uros_thread, NULL,
+                NULL, NULL, UROS_THREAD_PRIORITY, 0, 0);
+
+void uros_thread(void *param1, void *param2, void *param3) {
+  while (atomic_get(&status) == STATUS_STARTING) {
+    k_msleep(10);
+  }
+
+  rcl_allocator_t allocator = rcl_get_default_allocator();
+
+  // Initialize support. The loop with error handling is required, as the
+  // initialization will fail after some time if no agent is connected
+  // (on my stm32f429i-disc1 after ~55 seconds).
+  rclc_support_t support;
+  while (true) {
+    rcl_ret_t support_init_ret =
+        rclc_support_init(&support, 0, NULL, &allocator);
+
+    if (support_init_ret == RCL_RET_OK) {
+      break;
+    } else {
+      rclc_support_fini(&support);
+      if (support_init_ret != RCL_RET_ERROR) {
+        atomic_set(&status, STATUS_ERROR);
+        return;
+      }
+    }
+
+    k_msleep(100);
+  }
+
+  rcl_node_t node = rcl_get_zero_initialized_node();
+  RCCHECK(rclc_node_init_default(&node, "microros_zephyr_display_demo", "",
+                                 &support));
+
+  atomic_set(&status, STATUS_RUNNING);
+  while (1) {
+    k_msleep(1000);
+  }
+
+  RCCHECK(rcl_node_fini(&node));
+  RCCHECK(rclc_support_fini(&support));
+}
