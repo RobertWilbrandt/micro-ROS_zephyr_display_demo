24f0fb5e6fd1a6eeebcbce15cb1293231635f8df
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2020-11-21 21:12:10 +0100

    Publish dummy gyro temperature data periodically

--- apps/display_demo/src/gyro.c
+++ apps/display_demo/src/gyro.c
@@ -12,6 +12,7 @@
 
 #include "display.h"
 #include "uros.h"
+#include "status.h"
 
 // Do this if we have a real gyro
 #define GYRO_NODE DT_ALIAS(gyro)
@@ -23,7 +24,15 @@
 
 const struct device* gyro_dev;
 
+#define GYRO_THREAD_STACK_SIZE 2048
+#define GYRO_THREAD_PRIORITY 3
+
+void gyro_thread(void* param1, void* param2, void* param3);
+K_THREAD_STACK_DEFINE(gyro_thread_stack_area, GYRO_THREAD_STACK_SIZE);
+struct k_thread gyro_thread_stack_data;
+
 uros_add_pub_node_t uros_temp_pub_node;
+size_t uros_temp_pub_idx;
 
 int gyro_init()
 {
@@ -37,7 +46,7 @@ int gyro_init()
   // Create uros publisher
   uros_temp_pub_node.type = ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Temperature);
   uros_temp_pub_node.topic_name = "l3gd20_temp";
-  uros_add_publisher(&uros_temp_pub_node);
+  uros_temp_pub_idx = uros_add_publisher(&uros_temp_pub_node);
   return 0;
 }
 
@@ -47,7 +56,39 @@ int gyro_start()
   sprintf(gyro_label_text, "Using onboard sensor '%s'", GYRO_LABEL);
   display_set_label_gyro(gyro_label_text);
 
+  k_thread_create(&gyro_thread_stack_data, gyro_thread_stack_area, K_THREAD_STACK_SIZEOF(gyro_thread_stack_area),
+                  &gyro_thread, NULL, NULL, NULL, GYRO_THREAD_PRIORITY, 0, K_NO_WAIT);
+
   return 0;
+}
+
+void gyro_thread(void* param1, void* param2, void* param3)
+{
+  (void)param1;
+  (void)param2;
+  (void)param3;
+
+  sensor_msgs__msg__Temperature temp_msg = { 0 };
+  temp_msg.header.frame_id.data = "test_frame";
+  temp_msg.header.frame_id.size = strlen(temp_msg.header.frame_id.data);
+  temp_msg.variance = 0;
+  temp_msg.temperature = 42.0;
+
+  while (atomic_get(&status) != STATUS_RUNNING)
+  {
+    k_sleep(K_MSEC(100));
+  }
+
+  while (true)
+  {
+    if (uros_publish(uros_temp_pub_idx, &temp_msg) != RCL_RET_OK)
+    {
+      atomic_set(&status, STATUS_ERROR);
+      return;
+    }
+
+    k_sleep(K_MSEC(500));
+  }
 }
 
 // We don't have a gyro -> listen to uros instead
