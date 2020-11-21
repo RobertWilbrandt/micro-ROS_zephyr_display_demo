f2f4599619c6c93cff82b40d235ff7d350a93cd2
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2020-11-21 20:33:44 +0100

    Add infrastructure to register publishers

--- apps/display_demo/src/uros.c
+++ apps/display_demo/src/uros.c
@@ -3,6 +3,8 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
+#include "uros.h"
+
 #include <rcl/rcl.h>
 #include <rclc/rclc.h>
 #include <zephyr.h>
@@ -27,6 +29,16 @@
 
 #include "status.h"
 
+// List of publishers to create
+sys_slist_t uros_pub_list = SYS_SFLIST_STATIC_INIT(&uros_pub_list);
+size_t uros_pub_list_cnt = 0;
+
+size_t uros_add_publisher(uros_add_pub_node_t* node)
+{
+  sys_slist_append(&uros_pub_list, &node->node);
+  return uros_pub_list_cnt++;
+}
+
 // Thread structures
 void uros_thread(void* param1, void* param2, void* param3);
 K_THREAD_STACK_DEFINE(uros_thread_stack_area, UROS_THREAD_STACK_SIZE);
@@ -50,6 +62,20 @@ void uros_thread(void* param1, void* param2, void* param3)
   (void)param2;
   (void)param3;
 
+  // Start by allocating all required support structures
+  rcl_publisher_t* uros_publishers = NULL;
+  if (uros_pub_list_cnt > 0)
+  {
+    uros_publishers = k_malloc(uros_pub_list_cnt * sizeof(rcl_publisher_t));
+    if (uros_publishers == NULL)
+    {
+      atomic_set(&status, STATUS_ERROR);
+      return;
+    }
+    memset(uros_publishers, 0, uros_pub_list_cnt * sizeof(rcl_publisher_t));
+  }
+
+  // Start uros initialization
   rcl_allocator_t allocator = rcl_get_default_allocator();
 
   // Initialize support. The loop with error handling is required, as the
@@ -79,6 +105,28 @@ void uros_thread(void* param1, void* param2, void* param3)
 
   rcl_node_t node = rcl_get_zero_initialized_node();
   RCCHECK(rclc_node_init_default(&node, "microros_zephyr_display_demo", "", &support));
+
+  // Add publishers
+  size_t cur_node_i = 0;
+  for (uros_add_pub_node_t* cur_node = SYS_SLIST_PEEK_HEAD_CONTAINER(&uros_pub_list, cur_node, node); cur_node != NULL;
+       cur_node = SYS_SLIST_PEEK_NEXT_CONTAINER(cur_node, node))
+  {
+    rcl_ret_t pub_init_ret =
+        rclc_publisher_init_default(&uros_publishers[cur_node_i], &node, cur_node->type, cur_node->topic_name);
+    if (pub_init_ret != RCL_RET_OK)
+    {
+      atomic_set(&status, STATUS_ERROR);
+      return;
+    }
+    // RCCHECK(rclc_publisher_init_default(&uros_publishers[cur_node_i], &node, cur_node->type, cur_node->topic_name));
+    ++cur_node_i;
+  }
+
+  if (cur_node_i != uros_pub_list_cnt)
+  {
+    atomic_set(&status, STATUS_ERROR);
+    return;
+  }
 
   atomic_set(&status, STATUS_RUNNING);
   while (1)
--- apps/display_demo/src/uros.h
+++ apps/display_demo/src/uros.h
@@ -1,6 +1,18 @@
 #ifndef MICRO_ROS_ZEPHYR_DISPLAY_DEMO_UROS_H
 #define MICRO_ROS_ZEPHYR_DISPLAY_DEMO_UROS_H
 
+#include <rclc/rclc.h>
+#include <zephyr.h>
+
+typedef struct
+{
+  sys_snode_t node;
+  const rosidl_message_type_support_t* type;
+  const char* topic_name;
+} uros_add_pub_node_t;
+
+size_t uros_add_publisher(uros_add_pub_node_t* node);
+
 int uros_init();
 int uros_start();
 
